d806c7a23907cd56f24ddec6eaa9e138ef76aa28
CallumCooper <ccooper1982@proton.me>
2025-01-11 02:27:21 +0000

    get(): key not present test + fix

--- apis/python/fc/kv.py
+++ apis/python/fc/kv.py
@@ -64,7 +64,7 @@ class KV:
 
       # this is how we get a flexbuffer from a flatbuffer
       result = flatbuffers.flexbuffers.Loads(union_body.KvAsNumpy().tobytes())
-      if isSingleKey and len(result):
+      if isSingleKey:
         return result[key] if key in result else None  # single key, so return value
       else:
         return result # multiple keys, return dict
